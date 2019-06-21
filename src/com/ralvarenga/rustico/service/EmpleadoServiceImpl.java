58200ef5d58a4f592902e283aa49433ba63319d0
tupic98 <00017316@uca.edu.sv>
2019-06-21 07:34:00 -0600

    Implementing employee service and injecting employee repository methods respectively

---
+++ src/com/ralvarenga/rustico/service/EmpleadoServiceImpl.java
@@ -0,0 +1,37 @@
+package com.ralvarenga.rustico.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ralvarenga.rustico.domain.Empleado;
+import com.ralvarenga.rustico.repository.EmpleadoRepository;
+
+@Service
+public class EmpleadoServiceImpl implements EmpleadoService {
+
+	@Autowired
+	EmpleadoRepository empleadoRepository;
+
+	@Override
+	public List<Empleado> findAllEmployees() {
+		return empleadoRepository.findAll();
+	}
+
+	@Override
+	public Empleado findEmployeeById(Long id) {
+		return empleadoRepository.findOne(id);
+	}
+
+	@Override
+	public Empleado saveEmployee(Empleado empleado) {
+		return empleadoRepository.save(empleado);
+	}
+
+	@Override
+	public void deleteEmployee(Long id) {
+		empleadoRepository.delete(id);
+	}
+
+}
