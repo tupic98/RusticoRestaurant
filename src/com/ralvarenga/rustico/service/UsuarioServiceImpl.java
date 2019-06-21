cf17077b745b280fd59ec658931722d373fced85
tupic98 <00017316@uca.edu.sv>
2019-06-21 07:35:33 -0600

    Implementing user service and injecting user repository methods respectively

---
+++ src/com/ralvarenga/rustico/service/UsuarioServiceImpl.java
@@ -0,0 +1,32 @@
+package com.ralvarenga.rustico.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ralvarenga.rustico.domain.Usuario;
+import com.ralvarenga.rustico.repository.UsuarioRepository;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+	@Autowired
+	UsuarioRepository usuarioRepository;
+
+	@Override
+	public Usuario loginUsuario(String usuario, String password) {
+		return usuarioRepository.findByUsuarioAndClave(usuario, password);
+	}
+
+	@Override
+	public List<Usuario> findAllUsers() {
+		return usuarioRepository.findAll();
+	}
+
+	@Override
+	public Usuario getUserById(Long id) {
+		return usuarioRepository.findOne(id);
+	}
+
+}
