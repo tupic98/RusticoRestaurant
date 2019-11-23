7e3be41af64ab827df4d606d19bb0dfdd1c03ded
Rodrigo Alvarenga <00017316@uca.edu.sv>
2019-11-23 04:04:58 -0600

    Update host

--- src/com/ralvarenga/rustico/configuration/JPAConfiguration.java
+++ src/com/ralvarenga/rustico/configuration/JPAConfiguration.java
@@ -37,7 +37,7 @@ public class JPAConfiguration {
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/tareaNCapas");
+		dataSource.setUrl("jdbc:postgresql://postgres:5432/tareaNCapas");
 		dataSource.setUsername("postgres");
 		dataSource.setPassword("root");
 		return dataSource;
